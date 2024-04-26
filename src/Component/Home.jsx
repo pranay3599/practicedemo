ed4022d056ff1bd1434e6fb2f510444c643a5eec
pranay3599 <pranaymangrudkar03@gmail.com>
2024-04-27 01:15:32 +0530

    changes in Home page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>wellcome to the Page</h1>
+      <h1>well come to the Home page </h1>
+      <h4>This is the Home page</h4>
+      <p>efi oehriw  oehwriuvljp hfwr jeorahgeio rigtv i. hiruoegt. ohriutgv hifgv iusghjbkjoegf rihf vgig i ifivhigrivhigif.  rigvi </p>
     </div>
   )
 }
